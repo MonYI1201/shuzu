a8e3e8c72e1e96f0398ecb6071781b89e6300e66
luolaizhou <you@example.com>
2021-02-21 21:00:20 +0800

    罗来洲
    系统控制类

--- src/com/demo/controller/Controller.java
+++ src/com/demo/controller/Controller.java
@@ -1,7 +1,30 @@
 package com.demo.controller;
+
+import com.demo.entity.Config;
+import com.demo.service.ArrayInsertService;
+import com.demo.service.ArraySortService;
+import com.demo.service.UserService;
+
 /*
  * 系统控制类（罗来州）
  */
 public class Controller {
+	
+	public void run(){
 
+		int b[] = UserService.Arr();
+		
+		
+		ArraySortService.buSort(b, 1);
+		
+		int index = UserService.Index();
+		
+		ArraySortService.partSort(b, Config.C.length, index-Config.NUMBER-1, 1);
+		
+		ArrayInsertService.arrayInsertService(Config.C, index, Config.NUMBER);
+		
+		ArrayInsertService.arrayInsert(Config.C, index);
+		
+		ArrayInsertService.arryAssignments(b, Config.C, index, index + Config.NUMBER - 1);
+	}
 }
--- src/com/demo/domain/Test.java
+++ src/com/demo/domain/Test.java
@@ -1,10 +1,14 @@
 package com.demo.domain;
+
+
+
 /*
  * ³ÌÐòÈë¿Ú
  */
 public class Test {
 	
 	public static void main(String[] args) {
-	
-}
+		com.demo.controller.Controller c = new com.demo.controller.Controller();
+		c.run();
+	}
 }
