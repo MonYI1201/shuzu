dac69aa6f48ec0af1cd1b01ddbe9ee7d81b675d0
Your Name <you@example.com>
2021-02-21 19:57:39 +0800

    付哲
    实现了数组的位移，和数组内容的替换

--- src/com/demo/service/ArrayInsertService.java
+++ src/com/demo/service/ArrayInsertService.java
@@ -2,6 +2,46 @@ package com.demo.service;
 /*
  * 数组插入类(数组移位)（付哲）
  */
-public class ArrayInsertService {
 
+public class ArrayInsertService {
+	
+	/*
+	 *  arr 数组的引用
+	 *  index 位移的起始下标
+	 *  nums 位移的个数
+	 */
+	public static void arrayInsertService(int arr[], int index, int nums) {
+		for(int i = arr.length - 1; i >= index + nums; i--) {
+			if(index == 5) {
+				arr[i] = arr[i - nums + 1];
+			}else {
+				arr[i] = arr[i - nums];
+			}
+		}
+	}
+	
+	
+	/*
+	 * 数组后移一个位置
+	 * 
+	 */
+	public static void arrayInsert(int arr[], int index) {
+		for(int f = arr.length; f < index; f++) {
+			arr[f] = arr[f - 1];
+		}
+	}
+	
+	/*
+	 * 将数组a指定长度的内容赋值给数组b
+	 * a 替换数据
+	 * b 原始数据
+	 * index 起始位置
+	 * lenth 长度
+	 */
+	public static void arryAssignments(int a[],int b [],int index, int lenth) {
+		for(int z = index; z < lenth; z++) {
+			b[z] = a[z];
+		}
+	}
+	
 }
